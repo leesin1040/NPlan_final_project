a5adb48cd3d30f41a2a3af4bf902fad91bb53f47
halbebe <gksmfsf97@naver.com>
2024-01-17 16:05:23 +0900

    ADD:[user] 초기세팅

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,23 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), AuthModule],
+  imports: [TypeOrmModule.forFeature([User]),
+  AuthModule, 
+  JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (ConfigService: ConfigService) => ({
+      secret: ConfigService.get<string>('JWT_SECRET'),
+      signOptions: {
+        expiresIn: '12h',
+      },
+    }),
+  }),
+],
   controllers: [UserController],
   providers: [UserService],
 })
