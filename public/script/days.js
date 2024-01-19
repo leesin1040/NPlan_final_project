077aa36b973945b712de2b0028e4b893dc8e82dc
한승준 <jk2tmdwns@naver.com>
2024-01-19 18:21:58 +0900

    UPDATE: [front/connect] 카카오 맵 | 경유지  동선 표시

--- public/script/days.js
+++ public/script/days.js
@@ -64,6 +64,121 @@ function viewDayPath(dayId) {
   // viewPathModal에 카드 ForEach하면서  위도경도 로 목적지 찍고 선긋기
   // 카카오는 한번에 5개경유지  if 5개가 넘어간다면 합쳐줘야한다
   // 일단 5개까지만 해보자
+
+  // 1.카카오 모빌리티에서 동선가져오기
+
+  const REST_API_KEY = ''; // 여기에 카카오디벨로퍼스에서 발급 받은 REST API 키 값을 넣어주세요
+
+  const data = {
+    // 출발지
+    origin: {
+      x: '127.11024293202674',
+      y: '37.394348634049784',
+    },
+    // 목적지
+    destination: {
+      x: '127.10860518470294',
+      y: '37.401999820065534',
+    },
+    // 경유지 순서
+    waypoints: [
+      {
+        name: 'name0',
+        x: 127.11341936045922,
+        y: 37.39639094915999,
+      },
+    ],
+    priority: 'RECOMMEND',
+    car_fuel: 'GASOLINE',
+    car_hipass: false,
+    alternatives: false,
+    road_details: false,
+  };
+
+  axios
+    .post('https://apis-navi.kakaomobility.com/v1/waypoints/directions', data, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `KakaoAK ${REST_API_KEY}`,
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      const ArrayRoads = response.data.routes[0].sections[0].roads;
+      console.log(ArrayRoads);
+      console.log(response.data.routes[0].sections);
+      const linePath = [];
+      response.data.routes[0].sections.forEach((a) => {
+        a.roads.forEach((router) => {
+          router.vertexes.forEach((vertex, index) => {
+            if (index % 2 === 0) {
+              linePath.push(
+                new kakao.maps.LatLng(router.vertexes[index + 1], router.vertexes[index]),
+              );
+            }
+          });
+        });
+      });
+
+      var polyline = new kakao.maps.Polyline({
+        path: linePath,
+        strokeWeight: 5,
+        strokeColor: '#000000',
+        strokeOpacity: 0.7,
+        strokeStyle: 'solid',
+      });
+      polyline.setMap(map);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  // 1.카카오 map에서 지도가져오기
+  let mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+      // 좌표에 카드들 중 첫번쨰의 좌표
+      center: new kakao.maps.LatLng(37.394348634049784, 127.11024293202674), // 지도의 중심좌표
+      level: 3,
+    };
+
+  // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+  let map = new kakao.maps.Map(mapContainer, mapOption);
+  console.log(data.destination.y);
+  // 마커 표시하기
+  // 카드들의 위치와 표시
+  // 마커를 표시할 위치와 title 객체 배열입니다
+  var positions = [
+    {
+      title: '출발지',
+      latlng: new kakao.maps.LatLng(data.origin.y, data.origin.x),
+    },
+    {
+      title: '경유지',
+      latlng: new kakao.maps.LatLng(data.waypoints[0].y, data.waypoints[0].x),
+    },
+    {
+      title: '도착지',
+      latlng: new kakao.maps.LatLng(String(data.destination.y), String(data.destination.x)),
+    },
+  ];
+  // 마커 이미지의 이미지 주소입니다
+  var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
+
+  for (var i = 0; i < positions.length; i++) {
+    // 마커 이미지의 이미지 크기 입니다
+    var imageSize = new kakao.maps.Size(24, 35);
+
+    // 마커 이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      map: map, // 마커를 표시할 지도
+      position: positions[i].latlng, // 마커를 표시할 위치
+      title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+      image: markerImage, // 마커 이미지
+    });
+  }
 }
 
 // 드래그 앤 드롭
