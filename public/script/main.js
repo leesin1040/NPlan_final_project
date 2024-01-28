1820c598f30ae0279e272b93811b9992b227dac2
leesin1040 <leesin1040@gmail.com>
2024-01-28 12:30:34 +0900

    UPDATE:[front/css]트레블 상세페이지 초기세팅

--- public/script/main.js
+++ public/script/main.js
@@ -6,34 +6,33 @@ async function createTravel() {
   let startDateString = document.getElementById('startDate').value;
   let endDateString = document.getElementById('endDate').value;
   let travelTitle = document.getElementById('boardTitle').value;
-  let travelColor = document.getElementById('color').value;
+  let travelColor = document.getElementById('selectedColor').value;
   let travelTheme = document.getElementById('theme').value;
   let travelRegion = document.getElementById('region').value;
-  // input type="date"에서 얻은 문자열을 Date 객체로 변환
+
   let startDate = new Date(startDateString);
   let endDate = new Date(endDateString);
   let timeDifferenceInMilliseconds = endDate - startDate;
   let days = timeDifferenceInMilliseconds / (1000 * 60 * 60 * 24);
-  let accessToken = localStorage.getItem('accessToken');
-  await axios({
-    method: 'POST',
-    url: 'api/travel',
-    data: {
+
+  try {
+    const createTravelResponse = await axios.post('api/travel', {
       title: travelTitle,
       color: travelColor,
       region: travelRegion,
       theme: travelTheme,
       start_date: startDateString,
       end_date: endDateString,
-    },
-    headers: { Authorization: `Bearer ${accessToken}` },
-  })
-    .then((response) => {
-      axios.post(`api/travel/${response.data.data.newTravel.id}/days`, { days: days });
-      window.location.href = `/travel/${response.data.data.newTravel.id}`;
-      // window.location.href = `/days.html?id=${response.data.data.newTravel.id}&title=${response.data.data.newTravel.title}`;
-    })
-    .catch((error) => {
-      alert(error.response.data.message);
     });
+
+    const travelId = createTravelResponse.data.data.newTravel.id;
+
+    // days가 전체 생성된 후에 이동하도록 await
+    await axios.post(`api/travel/${travelId}/days`, { days: days });
+
+    // days 생성 후 이동
+    window.location.href = `/travel/${travelId}`;
+  } catch (error) {
+    alert(error.response.data.message);
+  }
 }
