0fd5fcf3076bf5b8590913773113f509c70c5bdb
halbebe <gksmfsf97@naver.com>
2024-01-29 16:18:27 +0900

    FIX : [user] 비밀번호 수정 / 회원 탈퇴 수정함

--- public/script/userinfo.js
+++ public/script/userinfo.js
@@ -26,7 +26,7 @@ async function updateProfile() {
     console.log(response);
 
     // Only reload the page if the password change was successful
-    window.location.href = '/home';
+    window.location.reload();
   } catch (error) {
     alert('비밀번호 변경 중 오류가 발생했습니다. 다시 시도해주세요.');
     console.error('회원 정보 업데이트 중 오류 발생', error);
