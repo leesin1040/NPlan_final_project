fa8264d2ed7d9a891119b3f507a5f0b8689cfb4d
Han9526 <jk2tmdwns@naver.com>
2024-01-29 17:14:37 +0900

    UPDATE: [day] LexoRank 정렬 | map 경로표시

--- public/script/days.js
+++ public/script/days.js
@@ -99,84 +99,84 @@ window.onload = function () {
   });
 };
 // 스케쥴 상세보기 모달
-function viewScheduleDetail(scheduleId) {
-  axios.get(`api/schedule/one/${scheduleId}`).then((response) => {
-    // console.log(response.data);
-    const scheduleDetailData = response.data.data;
-    const scheduledayTitle = document.getElementById('dayTitle');
-    const scheduleName = document.getElementById('name');
-    const scheduleAddress = document.getElementById('address');
-    const scheduleCategory = document.getElementById('category');
-    const scheduleMemo = document.getElementById('memo');
-    const scheduleMap = document.getElementById('scheduleMap');
-    // day이름
-    scheduledayTitle.innerHTML += `${scheduleDetailData.day.day}일차`;
-    scheduleName.value = scheduleDetailData.place.name;
-    scheduleAddress.value = scheduleDetailData.place.address;
-    scheduleCategory.value = scheduleDetailData.place.category;
-    scheduleMemo.value = scheduleDetailData.memo;
+// function viewScheduleDetail(scheduleId) {
+//   axios.get(`api/schedule/one/${scheduleId}`).then((response) => {
+//     // console.log(response.data);
+//     const scheduleDetailData = response.data.data;
+//     const scheduledayTitle = document.getElementById('dayTitle');
+//     const scheduleName = document.getElementById('name');
+//     const scheduleAddress = document.getElementById('address');
+//     const scheduleCategory = document.getElementById('category');
+//     const scheduleMemo = document.getElementById('memo');
+//     const scheduleMap = document.getElementById('scheduleMap');
+//     // day이름
+//     scheduledayTitle.innerHTML += `${scheduleDetailData.day.day}일차`;
+//     scheduleName.value = scheduleDetailData.place.name;
+//     scheduleAddress.value = scheduleDetailData.place.address;
+//     scheduleCategory.value = scheduleDetailData.place.category;
+//     scheduleMemo.value = scheduleDetailData.memo;
 
-    // schedule map
-    // 일단은 위치 마커만 추가
-    // 추후 이전 스케쥴이 존재할 시 연결해서 동선 보여주기 추가
-    let mapContainer = scheduleMap, // 지도를 표시할 div
-      mapOption = {
-        // 좌표에 카드들 중 첫번쨰의 좌표
-        center: new kakao.maps.LatLng(scheduleDetailData.place.mapY, scheduleDetailData.place.mapX), // 지도의 중심좌표
-        level: 3,
-      };
+//     // schedule map
+//     // 일단은 위치 마커만 추가
+//     // 추후 이전 스케쥴이 존재할 시 연결해서 동선 보여주기 추가
+//     let mapContainer = scheduleMap, // 지도를 표시할 div
+//       mapOption = {
+//         // 좌표에 카드들 중 첫번쨰의 좌표
+//         center: new kakao.maps.LatLng(scheduleDetailData.place.mapY, scheduleDetailData.place.mapX), // 지도의 중심좌표
+//         level: 3,
+//       };
 
-    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-    let map = new kakao.maps.Map(mapContainer, mapOption);
-    // 마커 표시하기
-    // 카드들의 위치와 표시
-    // 마커를 표시할 위치와 title 객체 배열입니다
-    var positions = [
-      {
-        title: scheduleDetailData.place.name,
-        latlng: new kakao.maps.LatLng(scheduleDetailData.place.mapY, scheduleDetailData.place.mapX),
-      },
-    ];
-    // console.log(positions);
-    // 마커 이미지의 이미지 주소입니다
-    var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
+//     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+//     let map = new kakao.maps.Map(mapContainer, mapOption);
+//     // 마커 표시하기
+//     // 카드들의 위치와 표시
+//     // 마커를 표시할 위치와 title 객체 배열입니다
+//     var positions = [
+//       {
+//         title: scheduleDetailData.place.name,
+//         latlng: new kakao.maps.LatLng(scheduleDetailData.place.mapY, scheduleDetailData.place.mapX),
+//       },
+//     ];
+//     // console.log(positions);
+//     // 마커 이미지의 이미지 주소입니다
+//     var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
 
-    // 마커 이미지의 이미지 크기 입니다
-    var imageSize = new kakao.maps.Size(24, 35);
+//     // 마커 이미지의 이미지 크기 입니다
+//     var imageSize = new kakao.maps.Size(24, 35);
 
-    // 마커 이미지를 생성합니다
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+//     // 마커 이미지를 생성합니다
+//     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-      map: map, // 마커를 표시할 지도
-      position: positions[0].latlng, // 마커를 표시할 위치
-      title: positions[0].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-      image: markerImage, // 마커 이미지
-      clickable: true,
-    });
+//     // 마커를 생성합니다
+//     var marker = new kakao.maps.Marker({
+//       map: map, // 마커를 표시할 지도
+//       position: positions[0].latlng, // 마커를 표시할 위치
+//       title: positions[0].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+//       image: markerImage, // 마커 이미지
+//       clickable: true,
+//     });
 
-    // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(
-      marker,
-      'click',
-      (function (marker) {
-        var iwContent = `<div style = "width:100%; height:100%;">
-                             <div">${scheduleDetailData.place.name}</div>
-                            <div>${scheduleDetailData.place.address}</div>
-                            </div>`,
-          iwRemoveable = true;
-        var infowindow = new kakao.maps.InfoWindow({
-          content: iwContent,
-          removable: iwRemoveable,
-        });
-        return function () {
-          infowindow.open(map, marker);
-        };
-      })(marker),
-    );
-  });
-}
+//     // 마커에 클릭이벤트를 등록합니다
+//     kakao.maps.event.addListener(
+//       marker,
+//       'click',
+//       (function (marker) {
+//         var iwContent = `<div style = "width:100%; height:100%;">
+//                              <div">${scheduleDetailData.place.name}</div>
+//                             <div>${scheduleDetailData.place.address}</div>
+//                             </div>`,
+//           iwRemoveable = true;
+//         var infowindow = new kakao.maps.InfoWindow({
+//           content: iwContent,
+//           removable: iwRemoveable,
+//         });
+//         return function () {
+//           infowindow.open(map, marker);
+//         };
+//       })(marker),
+//     );
+//   });
+// }
 // 경로보기 모달창 이벤트리스너 할당 및 Get Map
 function viewDayPath(dayId) {
   const viewPathModal = document.getElementById('viewPathModal');
