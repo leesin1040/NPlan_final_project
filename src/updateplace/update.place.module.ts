7ea416e6caa3d1e9915695b4379a1c7e4d48a488
Han9526 <jk2tmdwns@naver.com>
2024-02-13 23:05:30 +0900

    FIX: [place] 오타수정

--- src/updateplace/update.place.module.ts
+++ src/updateplace/update.place.module.ts
@@ -6,12 +6,12 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { UpdatePlace } from './entitiy/update.place.entity';
 import { UpdatePlaceService } from './update.place.service';
 import { UpdatePlaceController } from './update.place.controller';
-import { TourApiPlace } from './entitiy/tourapi.entity';
+
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UpdatePlace, TourApiPlace], 'travelPlace'),
+    TypeOrmModule.forFeature([UpdatePlace], 'travelPlace'),
     TypeOrmModule.forFeature([Place]),
     ScheduleModule.forRoot(),
     HttpModule,
